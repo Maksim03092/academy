efa78dea4ad7b1c538f3ee3fea87f47187e1553a
Max <maksim.vysotski@yandex.by>
2022-06-16 19:22:59 +0300

    test

---
+++ src/main/java/by/academy/lesson3/classwork/Math.java
@@ -0,0 +1,14 @@
+package by.academy.lesson3.classwork;
+
+public class Math {
+
+    public static void main(String[] args) {
+
+        int a = 2_123_000, b = 100, c = 234;
+        int a1 = 3, b1 = 4;
+
+        System.out.println(a/b*c);
+        System.out.println("3*3+4*4="+(3*3+4*4));
+    }
+
+}
