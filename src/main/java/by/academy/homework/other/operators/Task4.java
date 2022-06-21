575b7db548a0d63264d16c6f886993078a013cbf
Max <maksim.vysotski@yandex.by>
2022-06-21 17:01:21 +0300

    test

--- src/main/java/by/academy/homework/other/operators/Task4.java
+++ src/main/java/by/academy/homework/other/operators/Task4.java
@@ -3,7 +3,7 @@ package by.academy.homework.other.operators;
 public class Task4 {
     public static void main(String[] args) {
 
-        int s = 370_000_000, sec, m, min, h, hour, day, d, week, w,mon, month, year;
+        int s = 370_000, sec, m, min, h, hour, day, d, week, w,mon, month, year;
 
         sec = s % 60;
 
@@ -13,19 +13,19 @@ public class Task4 {
 
         h = (m - min) / 60;
 
-        hour = h % 60;
+        hour = h % 24;
 
         d = (h - hour) / 24;
 
-        day = d % 24;
+        day = d % 7;
 
         w = (d - day) / 7;
 
-        week = w % 7;
+        week = w % 4;
 
         mon = (w - week) / 4;
 
-        month = mon % 4;
+        month = mon % 12;
 
         year = (mon - month) / 12;
 
