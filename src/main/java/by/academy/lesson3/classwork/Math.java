7a58d62c4f43d87feaccc0bedafd7c7994b7d25f
Max <maksim.vysotski@yandex.by>
2022-12-06 15:47:42 +0300

    test

--- src/main/java/by/academy/lesson3/classwork/Math.java
+++ src/main/java/by/academy/lesson3/classwork/Math.java
@@ -4,11 +4,15 @@ public class Math {
 
     public static void main(String[] args) {
 
-        int a = 2_123_000, b = 100, c = 234;
-        int a1 = 3, b1 = 4;
-
-        System.out.println(a/b*c);
-        System.out.println("3*3+4*4="+(3*3+4*4));
     }
 
+    public static String makeComplements(String dna) {
+
+        for (int i = 0; i < dna.length(); i++){
+
+
+
+        }
+        return dna;
+    }
 }
